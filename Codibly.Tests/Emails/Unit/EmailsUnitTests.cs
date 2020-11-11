58a65419b5d2f63e6b5ce0aba03b9d911a058ca3
Adam <adam.joka@gmail.com>
2020-11-11 11:54:00 +0100

    add controller unit test

---
+++ Codibly.Tests/Emails/Unit/EmailsUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using Codibly.Controllers;
+using Codibly.Domain.Emails.Model;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codibly.Tests.Emails.Unit
+{
+    public class EmailContollerUnitTests
+    {
+        [Fact]
+        public async Task Index_ReturnsAJsonResult_WithAListOfEmails()
+        {
+            //Arange
+            var mediator = new Mock<IMediator>();           
+
+            var emailController = new EmailController(mediator.Object);
+
+            var result = await emailController.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+    }
+}
